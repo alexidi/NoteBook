baa8d84dc7fbea23847cdbda6ede5e148b314977
alexidi <alex.vyrlan@gmail.com>
2024-07-08 01:33:22 +0300

    add setters

--- src/Notebook.java
+++ src/Notebook.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class Notebook {
     private final String brand;
-    private final int ram; // ОЗУ в ГБ
-    private final int hdd; // Объем ЖД в ГБ
-    private final String os; // Операционная система
-    private final String color;
+    private int ram; // ОЗУ в ГБ
+    private int hdd; // Объем ЖД в ГБ
+    private String os; // Операционная система
+    private String color;
 
     // Конструктор
     public Notebook(String brand, int ram, int hdd, String os, String color) {
@@ -35,6 +35,32 @@ class Notebook {
 
     public String getColor() {
         return color;
+    }
+
+    // Сеттеры
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // Метод для сравнения ноутбуков
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Notebook notebook = (Notebook) o;
+        return ram == notebook.ram && hdd == notebook.hdd && Objects.equals(brand, notebook.brand) && Objects.equals(os, notebook.os) && Objects.equals(color, notebook.color);
     }
 
     // Метод для вывода информации о ноутбуке
