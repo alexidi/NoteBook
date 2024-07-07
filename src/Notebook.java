9dbfb5c85e9ef2506416ad0e27d5f2d6284c05b2
alexidi <alex.vyrlan@gmail.com>
2024-07-08 00:40:27 +0300

    add new class Notebook

---
+++ src/Notebook.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Notebook {
+    private String brand;
+    private int ram; // ОЗУ в ГБ
+    private int hdd; // Объем ЖД в ГБ
+    private String os; // Операционная система
+    private String color;
+
+    // Конструктор
+    public Notebook(String brand, int ram, int hdd, String os, String color) {
+        this.brand = brand;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.os = os;
+        this.color = color;
+    }
+
+    // Геттеры
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    // Метод для вывода информации о ноутбуке
+    @Override
+    public String toString() {
+        return "Notebook{" +
+                "brand='" + brand + '\'' +
+                ", ram=" + ram +
+                ", hdd=" + hdd +
+                ", os='" + os + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
